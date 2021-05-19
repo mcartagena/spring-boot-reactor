f09a3b7e23b7ecb5ba77a901cd25a4a4e4eef3c0
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2021-05-19 17:51:29 -0400

    Adding operator filter to reactive stream publisher.

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -2,7 +2,6 @@ package com.bolsadeideas.springboot.reactor.app;
 
 import com.bolsadeideas.springboot.reactor.app.models.User;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,29 +18,29 @@ public class SpringBootReactorApplication implements CommandLineRunner {
     }
 
     @Override
-    public void run(String... args) throws Exception {
-        Flux<User> nombres = Flux.just("Cielo", "Soledad", "Leo", "Sayen")
-                .map(name -> {
-                    return new User(name.toUpperCase(),null);
-                } )
+    public void run(String... args) {
+        Flux<User> nombres = Flux.just("Cielo Cartagena",
+                "Soledad Cartagena",
+                "Leo Cartagena",
+                "Sayen Cartagena",
+                "Mariela Orellana",
+                "Solange Troy")
+                .map(name -> new User(name.split(" ")[0].toUpperCase(),
+                name.split(" ")[1].toUpperCase()))
+                .filter(user -> user.getLastname().equalsIgnoreCase("Cartagena"))
                 .doOnNext(user -> {
-                    if (Optional.ofNullable(user).isPresent()) {
-                        System.out.println(user);
-                    } else {
+                    if (Optional.ofNullable(user).isEmpty()) {
                         throw new RuntimeException("Nombres no pueden ser vacios");
+                    } else {
+                        System.out.println(user);
                     }
                 })
                 .map(user -> {
-                    String nombre = user.getName();
-                    user.setName(nombre.toLowerCase());
+                    String name = user.getName();
+                    user.setName(name.toLowerCase());
                     return user;
                 });
 
-        nombres.subscribe(user -> log.info(user.toString()), error -> log.error(error.getMessage()), new Runnable() {
-            @Override
-            public void run() {
-                log.info("Ha finalizado la ejecucion del observable con exito!");
-            }
-        });
+        nombres.subscribe(user -> log.info(user.toString()), error -> log.error(error.getMessage()), () -> log.info("Ha finalizado la ejecucion del observable con exito!"));
     }
 }
